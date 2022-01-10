ae6ae1c12fbb95effb056b9925586e5569bf902d
Jon Taylor <31380713+DevJonTaylor@users.noreply.github.com>
2022-01-09 21:36:43 -0600

    Added an object to generate the password.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,113 @@
 // Assignment code here
-// TODO: Prompt for length
-// TODO: Prompt for uppercase
-// TODO: Prompt for lowercase
-// TODO: Prompt for Numeric
-// TODO: Prompt for Special Characters.
-// TODO: Validation should be length of 8-128 characters and at least one character type.
-// TODO: Validations are passed then write the password to the page.
 
-// Get references to the #generate element
+/**
+ * @var generateBtn
+ * @type {Element}
+ * @define reference to the button that generates the password
+ */
 const generateBtn = document.querySelector('#generate');
+
+/**
+ * @var passwordText
+ * @type {Element}
+ * @define reference to the element that will display the password.
+ */
 const passwordText = document.querySelector('#password');
 
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+/**
+ * @define A class to easily contain and access all password generating code.
+ */
 class PasswordGenerator {
+  /**
+   * Constructor for the class.
+   * @param {boolean} isLower Lowercase is in use.
+   * @param {boolean} isUpper Uppercase is in use.
+   * @param {boolean} isSpecial Special characters are in use.
+   * @param {boolean} isNumeric Numbers are in use.
+   * @param {number} length How long the password will be.
+   */
   constructor(isLower, isUpper, isSpecial, isNumeric, length) {
     this.isLower = isLower;
     this.isUpper = isUpper;
     this.isSpecial = isSpecial;
     this.isNumeric = isNumeric;
     this.length = length;
-    this.password = this.newPassword();
+    this.characters = '';
+    this.password = '';
+
+    this.getCharacters();
+    this.newPassword();
   }
 
-  get characters() {
-    let characters = '';
-    characters += this.isLower ? this.getLetters() : '';
-    characters += this.isUpper ? this.getLetters(true) : '';
-    characters += this.isSpecial ? this.getSpecial() : '';
-    characters += this.isNumeric ? this.getNumeric() : '';
-
-    return characters;
+  /**
+   * A method to get all characters that are needed.
+   */
+  getCharacters() {
+    this.characters = '';
+    this.characters += this.isLower ? this.getLetters() : '';
+    this.characters += this.isUpper ? this.getLetters(true) : '';
+    this.characters += this.isSpecial ? this.getSpecial() : '';
+    this.characters += this.isNumeric ? this.getNumeric() : '';
   }
 
+  /**
+   * Takes all given arguments and creates a new password.
+   */
   newPassword() {
-    let password = '';
+    this.password = '';
 
+    for(let i = 0; i < this.length; i++)
+      this.password += this.characters[randomNumber(0, this.characters.length - 1)];
   }
 
+  /**
+   * Returns a-z|A-Z characters.
+   * @param {boolean} isUpper The characters returned will be uppercase.  This is false by default.
+   * @returns {string|string}
+   */
   getLetters(isUpper = false) {
     const letters = 'abcdefghijklmnopqrstuvwxyz';
     return isUpper ? letters.toUpperCase() : letters;
   }
 
+  /**
+   * Returns a string of special characters.
+   * @returns {string}
+   */
   getSpecial() {
     return ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
   }
 
+  /**
+   * Returns a string of numeric characters.
+   * @returns {string}
+   */
   getNumeric() {
     return '01234567890';
   }
-
-
-}
-
-function getCharacters(isLower, isUpper, isSpecial, isNumeric) {
-  let includedCharacters = '';
-  if(isLower) {
-    includedCharacters += letters;
-  }
-  if(isUpper) {
-    includedCharacters += letters.toUpperCase();
-  }
-  if(isSpecial) {
-    includedCharacters += special;
-  }
-  if(isNumeric) {
-    includedCharacters += numbers
-  }
-
-  return includedCharacters;
-}
-
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function generatePassword(characters, length) {
-  let returnMe = '';
-
-  for(let i = 0; i < length; i++) {
-    returnMe += characters[randomNumber(0, characters.length - 1)];
-  }
-
-  if(!passwordCheck(returnMe, isLower, isUpper, ))
-  return returnMe;
-}
-
-function createPassword(isLower, isUpper, isSpecial, isNumeric, length) {
-  let characters = getCharacters(isLower, isUpper, isSpecial, isNumeric);
-  let password = generatePassword(characters, length, isLower, isUpper, isSpecial, isNumeric);
-
-
 }
 
 // Write password to the #password input
 function writePassword() {
   const length = prompt('How long would you like the password to be?  (8-128)');
-  const isLower = confirm('Would you like to include lowercase characters?');
-  const isUpper = confirm('Would you like to include uppercase characters?');
-  const isSpecial = confirm('Would you like to include special characters?');
-  const isNumeric = confirm('Would you like to include numerical characters?');
 
   if(length < 8 || length > 128) {
     alert(`The password length must be between 8 and 128 characters.  ${length} is not valid`);
     return writePassword();
   }
 
-  passwordText.value = createPassword(isLower, isUpper, isSpecial, isNumeric, length);
+  const isLower = confirm('Would you like to include lowercase characters?');
+  const isUpper = confirm('Would you like to include uppercase characters?');
+  const isSpecial = confirm('Would you like to include special characters?');
+  const isNumeric = confirm('Would you like to include numerical characters?');
+
+  const passGen = new PasswordGenerator(isLower, isUpper, isSpecial, isNumeric, length);
+
+  passwordText.value = passGen.password;
 }
 
 // Add event listener to generate button
