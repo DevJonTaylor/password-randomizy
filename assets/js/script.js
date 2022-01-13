ca5f0a0dc3604ebdad92c6d9f02d54747e2641ca
Jon Taylor <31380713+DevJonTaylor@users.noreply.github.com>
2022-01-13 04:51:42 -0600

    Updated JavaScript codebase documentation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-// Assignment code here
-
 /**
  * @var generateBtn
  * @type {Element}
@@ -14,6 +12,12 @@ const generateBtn = document.querySelector('#generate');
  */
 const passwordText = document.querySelector('#password');
 
+/**
+ * A random number generator.
+ * @param {number} min the min number you wish to get.
+ * @param {number} max The max number you wish to get.
+ * @returns {number}
+ */
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
@@ -39,7 +43,7 @@ class PasswordGenerator {
   }
 
   /**
-   * Takes all given arguments and creates a new password.
+   * Utilizing the Character object we create a custom, random, password.
    */
   newPassword() {
     let password = '';
@@ -54,35 +58,62 @@ class PasswordGenerator {
   }
 }
 
+/**
+ * one or more character types were not being included in the password even though they were selected.
+ * So this class was created to ensure that a random character for each type is included.
+ * Each character type is managed, and we use states to ensure that they are delivered as evenly as possible.
+ */
 class Characters {
   #states = [];
   #currentState = 0;
   #maxState = 0;
 
+  /**
+   * Constructor for this class.
+   * @param {boolean} isLower
+   * @param {boolean} isUpper
+   * @param {boolean} isSpecial
+   * @param {boolean} isNumeric
+   */
   constructor(isLower, isUpper, isSpecial, isNumeric) {
+    // Checking if these character types are needed.
     if(isLower) this.#states.push('abcdefghijklmnopqrstuvwxyz');
     if(isUpper) this.#states.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     if(isSpecial) this.#states.push(' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~');
     if(isNumeric) this.#states.push('0123456789');
 
+    // Setting a max length for the state array.
     this.#maxState = this.#states.length - 1;
-    // TODO:  Iterate through state.
-    // TODO:  Remove states that are not requested.
   }
 
+  /**
+   * This getter is used to retrieve the current character type that is being focused.
+   * @returns {string}
+   */
   get #charList() {
     return this.#states[this.#currentState];
   }
 
+  /**
+   * An easy access to a random index selected based on the current character type of focus.
+   * @returns {number}
+   */
   get #randomIndex() {
-    return randomNumber(0, this.#charList.length - 1);
+    return randomNumber(0, this.#charList.length);
   }
 
+  /**
+   * This method ticks from one character type to the next.  Once we have reached the last one it will start over.
+   */
   #tickState() {
     if(this.#currentState === this.#maxState) this.#currentState = 0;
     else this.#currentState++;
   }
 
+  /**
+   * Returns a single character inside a string then moves onto the next character type if one is available.
+   * @returns {string}
+   */
   get char() {
     let char = this.#charList[this.#randomIndex];
     this.#tickState();
